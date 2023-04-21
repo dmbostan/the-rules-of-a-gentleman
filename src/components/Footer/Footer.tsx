b6f7aef0e0fd26a960d6e2d3f70945bd270b04d1
Dmitri Bostan <dm.bostan@gmail.com>
2023-04-21 19:06:54 +0200

    Add unstyled footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+    return(
+        <footer>
+            <ul>
+                <li>
+                    <a href="#">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#">Terms & Conditions</a>
+                </li>
+                <li>
+                    <a href="#">Contact us</a>
+                </li>
+            </ul>
+        </footer>
+    );
+};
+
+export default Footer;
